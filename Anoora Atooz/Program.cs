68e9abd3e54693a043a15ca9043a5f14c9430c9f
Talal Khan <FA20-BCS-029@cuiatd.edu.pk>
2023-11-12 18:06:14 +0500

    Start

--- Anoora Atooz/Program.cs
+++ Anoora Atooz/Program.cs
@@ -15,20 +15,5 @@ namespace Anoora_Atooz
 
         }
 
-        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
-        {
-            MessageBox.Show(e.Exception.Message, "Unhandled Thread Exception");
-        }
-        // function to handle UI thread exceptions
-        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            MessageBox.Show((e.ExceptionObject as Exception).Message, "Unhandled UI Exception");
-        }
-
-        // function to print hello
-        private static void PrintHello()
-        {
-            Console.WriteLine("Hello");
-        }
     }
 }
