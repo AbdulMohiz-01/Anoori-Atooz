4628cd17f9de5ef5b74be80e0d0d13b79e5831d4
Abdul Mohiz <FA20-BCS-001@cuiatd.edu.pk>
2023-11-13 18:54:53 +0500

    add: nav buttons

--- Anoora Atooz/Views/Home.Designer.cs
+++ Anoora Atooz/Views/Home.Designer.cs
@@ -32,11 +32,22 @@
         {
             panel3 = new Panel();
             btnHome = new CustomControls.RJControls.RJButton();
+            btnCompany = new CustomControls.RJControls.RJButton();
+            btnProduct = new CustomControls.RJControls.RJButton();
+            btnHistory = new CustomControls.RJControls.RJButton();
+            btnInvoice = new CustomControls.RJControls.RJButton();
+            panel3.SuspendLayout();
             SuspendLayout();
             // 
             // panel3
             // 
             panel3.Anchor = AnchorStyles.Top;
+            panel3.BackColor = Color.FromArgb(10, 9, 12);
+            panel3.Controls.Add(btnInvoice);
+            panel3.Controls.Add(btnHistory);
+            panel3.Controls.Add(btnProduct);
+            panel3.Controls.Add(btnCompany);
+            panel3.Controls.Add(btnHome);
             panel3.Location = new Point(12, 12);
             panel3.Name = "panel3";
             panel3.Size = new Size(1505, 126);
@@ -44,30 +55,107 @@
             // 
             // btnHome
             // 
-            btnHome.BackColor = Color.RoyalBlue;
-            btnHome.BackgroundColor = Color.RoyalBlue;
-            btnHome.BorderColor = Color.Blue;
+            btnHome.BackColor = Color.FromArgb(7, 57, 60);
+            btnHome.BackgroundColor = Color.FromArgb(7, 57, 60);
+            btnHome.BorderColor = Color.FromArgb(44, 102, 110);
             btnHome.BorderRadius = 10;
-            btnHome.BorderSize = 1;
+            btnHome.BorderSize = 2;
             btnHome.FlatAppearance.BorderSize = 0;
             btnHome.FlatStyle = FlatStyle.Flat;
             btnHome.Font = new Font("Tahoma", 20F, FontStyle.Regular, GraphicsUnit.Point);
             btnHome.ForeColor = Color.White;
-            btnHome.Location = new Point(459, 227);
+            btnHome.Location = new Point(20, 26);
             btnHome.Name = "btnHome";
-            btnHome.Size = new Size(190, 68);
+            btnHome.Size = new Size(189, 74);
             btnHome.TabIndex = 17;
-            btnHome.Text = "Home";
+            btnHome.Text = "Home ";
             btnHome.TextColor = Color.White;
             btnHome.UseVisualStyleBackColor = false;
+            btnHome.Click += btnHome_Click;
+            // 
+            // btnCompany
+            // 
+            btnCompany.BackColor = Color.FromArgb(7, 57, 60);
+            btnCompany.BackgroundColor = Color.FromArgb(7, 57, 60);
+            btnCompany.BorderColor = Color.FromArgb(44, 102, 110);
+            btnCompany.BorderRadius = 10;
+            btnCompany.BorderSize = 2;
+            btnCompany.FlatAppearance.BorderSize = 0;
+            btnCompany.FlatStyle = FlatStyle.Flat;
+            btnCompany.Font = new Font("Tahoma", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCompany.ForeColor = Color.White;
+            btnCompany.Location = new Point(317, 26);
+            btnCompany.Name = "btnCompany";
+            btnCompany.Size = new Size(189, 74);
+            btnCompany.TabIndex = 18;
+            btnCompany.Text = "Company";
+            btnCompany.TextColor = Color.White;
+            btnCompany.UseVisualStyleBackColor = false;
+            // 
+            // btnProduct
+            // 
+            btnProduct.BackColor = Color.FromArgb(7, 57, 60);
+            btnProduct.BackgroundColor = Color.FromArgb(7, 57, 60);
+            btnProduct.BorderColor = Color.FromArgb(44, 102, 110);
+            btnProduct.BorderRadius = 10;
+            btnProduct.BorderSize = 2;
+            btnProduct.FlatAppearance.BorderSize = 0;
+            btnProduct.FlatStyle = FlatStyle.Flat;
+            btnProduct.Font = new Font("Tahoma", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            btnProduct.ForeColor = Color.White;
+            btnProduct.Location = new Point(639, 26);
+            btnProduct.Name = "btnProduct";
+            btnProduct.Size = new Size(189, 74);
+            btnProduct.TabIndex = 19;
+            btnProduct.Text = "Samaan";
+            btnProduct.TextColor = Color.White;
+            btnProduct.UseVisualStyleBackColor = false;
+            // 
+            // btnHistory
+            // 
+            btnHistory.BackColor = Color.FromArgb(7, 57, 60);
+            btnHistory.BackgroundColor = Color.FromArgb(7, 57, 60);
+            btnHistory.BorderColor = Color.FromArgb(44, 102, 110);
+            btnHistory.BorderRadius = 10;
+            btnHistory.BorderSize = 2;
+            btnHistory.FlatAppearance.BorderSize = 0;
+            btnHistory.FlatStyle = FlatStyle.Flat;
+            btnHistory.Font = new Font("Tahoma", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            btnHistory.ForeColor = Color.White;
+            btnHistory.Location = new Point(966, 26);
+            btnHistory.Name = "btnHistory";
+            btnHistory.Size = new Size(189, 74);
+            btnHistory.TabIndex = 20;
+            btnHistory.Text = "Hisab Kitab";
+            btnHistory.TextColor = Color.White;
+            btnHistory.UseVisualStyleBackColor = false;
+            // 
+            // btnInvoice
+            // 
+            btnInvoice.BackColor = Color.FromArgb(7, 57, 60);
+            btnInvoice.BackgroundColor = Color.FromArgb(7, 57, 60);
+            btnInvoice.BorderColor = Color.FromArgb(44, 102, 110);
+            btnInvoice.BorderRadius = 10;
+            btnInvoice.BorderSize = 2;
+            btnInvoice.FlatAppearance.BorderSize = 0;
+            btnInvoice.FlatStyle = FlatStyle.Flat;
+            btnInvoice.Font = new Font("Tahoma", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            btnInvoice.ForeColor = Color.White;
+            btnInvoice.Location = new Point(1288, 26);
+            btnInvoice.Name = "btnInvoice";
+            btnInvoice.Size = new Size(189, 74);
+            btnInvoice.TabIndex = 21;
+            btnInvoice.Text = "Slip";
+            btnInvoice.TextColor = Color.White;
+            btnInvoice.UseVisualStyleBackColor = false;
             // 
             // Home
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            BackColor = Color.FromArgb(10, 9, 12);
             ClientSize = new Size(1529, 811);
-            Controls.Add(btnHome);
             Controls.Add(panel3);
             MaximizeBox = false;
             MinimizeBox = false;
@@ -75,6 +163,8 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Anoora Atooz";
             WindowState = FormWindowState.Maximized;
+            Load += Home_Load;
+            panel3.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -85,5 +175,9 @@
         private Button buttonProdcuts;
         private Button buttonCompany;
         private CustomControls.RJControls.RJButton btnHome;
+        private CustomControls.RJControls.RJButton btnHistory;
+        private CustomControls.RJControls.RJButton btnProduct;
+        private CustomControls.RJControls.RJButton btnCompany;
+        private CustomControls.RJControls.RJButton btnInvoice;
     }
 }
--- Anoora Atooz/Views/Home.cs
+++ Anoora Atooz/Views/Home.cs
@@ -21,5 +21,15 @@ namespace Anoora_Atooz
         {
 
         }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
